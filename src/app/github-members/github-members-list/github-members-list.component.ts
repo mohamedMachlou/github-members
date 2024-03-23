d66c4bdf38f92cc277bf5ade7e43150aba0b355e
MACHLOU <medmachlou@gmail.com>
2024-03-23 23:52:36 +0000

    235123032024

--- src/app/github-members/github-members-list/github-members-list.component.ts
+++ src/app/github-members/github-members-list/github-members-list.component.ts
@@ -1,37 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { MembersService } from '../services/members.service';
-import { Member } from '../interfaces/member';
-
+import { Member } from '../models/member';
 
 @Component({
   selector: 'app-github-members-list',
   templateUrl: './github-members-list.component.html',
-  styleUrl: './github-members-list.component.css'
+  styleUrl: './github-members-list.component.css',
 })
-export class GithubMembersListComponent implements OnInit{
-users : Member[] = [];
-search : string = "";
-  constructor(private membersService: MembersService){}
+export class GithubMembersListComponent implements OnInit {
+  users: Member[] = [];
+  search: string = '';
+  constructor(private membersService: MembersService) {}
 
   ngOnInit(): void {
-    this.getAllMemebers() ; 
+    this.getAllMemebers();
   }
 
   getAllMemebers() {
     this.membersService.getMmembers().subscribe((res) => {
       this.users = res;
       console.log(this.users);
-    })
+    });
   }
 
   searchUsers() {
-    if(this.search) {
-      this.membersService
-        .searchMember(this.search)
-        .subscribe((res) => {this.users = res.items;  })
-    }else{
+    if (this.search) {
+      this.membersService.searchMember(this.search).subscribe((res) => {
+        this.users = res.items;
+      });
+      this.getAllMemebers();
+      this.search = '';
+    } else {
       this.getAllMemebers();
     }
-
   }
 }
--- src/app/github-members/services/members.service.ts
+++ src/app/github-members/services/members.service.ts
@@ -1,35 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
-import { Member } from '../interfaces/member';
-import { SearchUsers } from '../interfaces/search-users';
+import { Member } from '../models/member';
+import { SearchUsers } from '../models/search-users';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MembersService {
-  apiUrl = "https://api.github.com/users"
-  constructor( private http: HttpClient) { }
+  apiUrl = 'https://api.github.com/users';
+  constructor(private http: HttpClient) {}
 
   getMmembers(): Observable<Member[]> {
-    return this.http.get<Member[]>(this.apiUrl).pipe(map((users)=> 
+    return this.http.get<Member[]>(this.apiUrl).pipe(
+      map((users) =>
         users.map((user) => {
           return {
             avatar_url: user.avatar_url,
             html_url: user.html_url,
             login: user.login,
-             }
-          })
-        )
+          };
+        })
+      )
     );
   }
 
-  searchMember(member : string): Observable<SearchUsers> {
-    return this.http.get<SearchUsers>(`https://api.github.com/search/users?q= ${member}`)
+  searchMember(member: string): Observable<SearchUsers> {
+    return this.http.get<SearchUsers>(
+      `https://api.github.com/search/users?q= ${member}`
+    );
   }
-  
-
-
-
-
 }
